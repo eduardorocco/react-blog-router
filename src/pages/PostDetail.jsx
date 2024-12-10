a5e97b96381af770d5d58a7f20fc8d5adc4577bb
eduardorocco <eduardo_rocco@hotmail.it>
2024-12-10 16:24:01 +0100

    ADD prev and next buttons

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
@@ -5,9 +6,10 @@ import { useParams } from 'react-router-dom'
 const API_BASE_URI = 'http://localhost:3000/'
 
 export default function PostDetail() {
-    const { postId } = useParams()  
-    const [post, setPost] = useState(null)  
-    const [error, setError] = useState(null)  
+    const { postId } = useParams()
+    const [post, setPost] = useState(null)
+    const [posts, setPosts] = useState([])
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         async function fetchPost() {
@@ -20,24 +22,54 @@ export default function PostDetail() {
             }
         }
 
-        fetchPost() 
-    }, [postId])  
+        async function fetchAllPosts() {
+            try {
+                const response = await axios.get(`${API_BASE_URI}posts`)
+                setPosts(response.data)
+            } catch (error) {
+                console.error(error)
+                setError('Errore nel recuperare la lista dei post')
+            }
+        }
 
-   
+        fetchPost()
+        fetchAllPosts()
+    }, [postId])
+
+
     if (error) {
-        return <div>{error}</div>  
+        return <div>{error}</div>
     }
 
     if (!post) {
-        return <div>Post non trovato</div> 
+        return <div>Post non trovato</div>
     }
 
-    const {title, content, image} = post
+    const currentIndex = posts.findIndex(p => p.id === parseInt(postId))
+    const prevPost = posts[currentIndex - 1]
+    const nextPost = posts[currentIndex + 1]
+
+    const { title, content, image } = post
     return (
         <div className='container'>
             <h1>{title}</h1>
-            <img src={`${API_BASE_URI}imgs/posts/${image}`}/>
+            <img src={`${API_BASE_URI}imgs/posts/${image}`} />
             <p>{content}</p>
+
+            <div>
+                {prevPost && (
+                    <Link to={`/our-recipes/${prevPost.id}`} className="previousLink">
+                        Previous
+                    </Link>
+                )}
+
+                {nextPost && (
+                    <Link to={`/our-recipes/${nextPost.id}`} className="nextLink">
+                        Next
+                    </Link>
+                )}
+            </div>
+
         </div>
     )
 }
