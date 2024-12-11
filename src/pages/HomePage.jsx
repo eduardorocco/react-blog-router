106112b94825a48de0c345cf3c307d1b69e51bf5
eduardorocco <eduardo_rocco@hotmail.it>
2024-12-11 10:16:04 +0100

    ADD random image to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,54 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const API_BASE_URI = 'http://localhost:3000/'
+
 export default function HomePage() {
+    function getRandomInt(min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min)) + min
+    }
+
+    // Genera un numero casuale tra 1 e 5
+    let randomInt = getRandomInt(1, 5)
+
+    const [posts, setPosts] = useState([])
+    const [error, setError] = useState('')
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        async function fetchAllPosts() {
+            try {
+                const response = await axios.get(`${API_BASE_URI}posts`)
+                setPosts(response.data)
+            } catch (error) {
+                console.error(error)
+                setError('Errore nel recuperare la lista dei post')
+            }
+        }
+
+        fetchAllPosts();
+    }, []);
+
+    useEffect(() => {
+        
+        if (posts.length > 0) {
+            const filteredPost = posts.find(post => post.id === randomInt)
+            setPost(filteredPost || null)
+        }
+    }, [posts])
+
     return (
         <>
-            <h1>Homepage</h1>
+            {post ? (
+                <div>
+                    <h2>{post.title}</h2>
+                    <img src={`${API_BASE_URI}imgs/posts/${post.image}`} alt="" />
+                </div>
+            ) : (
+                <p>Post non trovato per l'ID {randomInt}.</p>
+            )}
         </>
     )
 }
